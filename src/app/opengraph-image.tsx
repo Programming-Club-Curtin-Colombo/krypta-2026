ac86e5c56f36051b7e196cb8b6422b70cd6b7fd1
Aaron Fredrick <20932390@student.curtin.edu.au>
2026-07-08 22:38:53 +0530

    fix: change opengraph-image runtime to nodejs to fix vercel limit

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export const alt = "KRYPTA 2026 - Knowledge, Research & Yielding Parallel Technologies Arena";
 export const size = {
