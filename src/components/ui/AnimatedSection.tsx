1741b7196ea5d09eba0b2d5b6f7f4cd58142bafa
Aaron Fredrick <20932390@student.curtin.edu.au>
2026-07-11 19:54:42 +0530

    chore: refactor CI SARIF generation and fix framer-motion typings

--- src/components/ui/AnimatedSection.tsx
+++ src/components/ui/AnimatedSection.tsx
@@ -30,7 +30,7 @@ export function AnimatedSection({
       y: 0,
       transition: {
         duration: 0.8,
-        ease: [0.22, 1, 0.36, 1] as number[],
+        ease: [0.22, 1, 0.36, 1] as [number, number, number, number],
         delay,
       },
     },
